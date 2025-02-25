935007175ca5e9be352416b4b0f8fdec99d49434
DigoDuck <digoribeiro34@gmail.com>
2025-02-25 14:01:21 -0300

    Sistema de Login criado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,26 +1,50 @@
-
-import { v4 } from "uuid"
+import { v4 } from "uuid";
+import * as Yup from "yup";
 
 import User from "../models/User";
 
 class UserController {
-  async store (req, res){
-    const { name, email, password_hash, admin } = req.body
-    
+  async store(req, res) {
+    const schema = Yup.object({
+      name: Yup.string()
+        .matches(
+          /^[a-zA-ZÀ-ÿ\s]+$/,
+          "O nome não pode conter números ou caracteres especiais."
+        )
+        .required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(req.body, { abortEarly: false });
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
+
+    const { name, email, password, admin } = req.body;
+
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: "E-mail já cadastrado!" });
+    }
+
     const user = await User.create({
-        id: v4(),
-        name,
-        email,
-        password_hash,
-        admin
-      });
-    
-      return res.status(201).json({
-        id: user.id,
-        name,
-        email,
-        admin,
-      });
+      id: v4(),
+      name,
+      email,
+      password,
+      admin,
+    });
+
+    return res.status(201).json({
+      id: user.id,
+      name,
+      email,
+      admin,
+    });
   }
 }
 
