cff9fc415661277a8f6b255d104cc683bb50656d
DigoDuck <digoribeiro34@gmail.com>
2025-02-26 12:32:51 -0300

    Adiciona middleware de autenticação com JWT e configurações de token

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 import User from "../models/User";
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth";
 
 class SessionController {
   async store(req, res) {
@@ -36,7 +38,13 @@ class SessionController {
 
     return res
       .status(201)
-      .json({ id: user.id, name: user.name, email, admin: user.admin });
+      .json({
+        id: user.id,
+        name: user.name,
+        email,
+        admin: user.admin,
+        token: jwt.sign({ id: user.id }, authConfig.secret, { expiresIn: authConfig.expiresIn }),
+      });
   }
 }
 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import authConfig from "../config/auth";
+
+function authMiddleware(req, res, next) {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  const token = authToken.split(" ").at("1");
+
+  try {
+    const decoded = jwt.verify(token, authConfig.secret);
+    req.userId = decoded.id;
+} catch (err) {
+    return res.status(401).json({ error: "Token invalid" });
+}
+return next();
+}
+
+export default authMiddleware;
