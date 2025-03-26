010f05a1de9580188ad1420c8ee5f41c5aaa1db6
DigoDuck <digoribeiro34@gmail.com>
2025-03-25 21:38:37 -0300

    Adiciona seleção de status de pedidos na interface de administração; implementa formatação de data e atualiza estilos

---
+++ interface/devburger-interface/src/containers/Admin/Orders/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Select from 'react-select'
+
+export const ProductImage = styled.img`
+    height: 89px;
+    padding: 12px;
+    border-radius: 16px;
+`
+
+export const SelectStatus = styled(Select)`
+    width: 220px;
+`;
